7f4aa72202e54e191be2cd7e9455f091971ea780
Akhila Sulgante <asulgante39@gmail.com>
2022-09-26 01:59:35 -0700

    color change

--- script/darkMode.js
+++ script/darkMode.js
@@ -1,24 +1,24 @@
 function switchMode(mode) {
-  let headingClasses, bgClasses, descClasses, navBarClasses;
+  let bgClasses, descClasses, navBarClasses, heading;
   console.log(mode);
   if (mode === "dark") {
-    headingClasses = document.getElementsByClassName("text-info");
-    updateClasses(headingClasses, "text-danger");
     bgClasses = document.getElementsByClassName("bg-light");
     updateClasses(bgClasses, "bg-dark");
     descClasses = document.getElementsByClassName("text-dark");
     updateClasses(descClasses, "text-light");
     navBarClasses = document.getElementsByClassName("navBarlight");
     updateClasses(navBarClasses, "navBardark");
+    heading = document.getElementsByClassName("headerlight");
+    updateClasses(heading, "headerdark");
   } else {
-    headingClasses = document.getElementsByClassName("text-danger");
-    updateClasses(headingClasses, "text-info");
     bgClasses = document.getElementsByClassName("bg-dark");
     updateClasses(bgClasses, "bg-light");
     descClasses = document.getElementsByClassName("text-light");
     updateClasses(descClasses, "text-dark");
     navBarClasses = document.getElementsByClassName("navBardark");
     updateClasses(navBarClasses, "navBarlight");
+    heading = document.getElementsByClassName("headerdark");
+    updateClasses(heading, "headerlight");
   }
 }
 
